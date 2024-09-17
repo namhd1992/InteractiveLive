e4d999640b236831cdd2eb3a0fc13212c06e837a
namhd1992 <stpnghd.vn@gmail.com>
2024-09-17 13:32:35 +0700

    fix

--- src/app/component/game/game.js
+++ src/app/component/game/game.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {isMobile, isDesktop, isTablet} from "react-device-detect"
+import {isMobile, isDesktop, isTablet} from "react-device-detect";
+import {data, img} from "../../ultils/data";
 
 
 class Game extends React.Component{
@@ -8,10 +9,23 @@ class Game extends React.Component{
 		super(props);
 		this.state = {
             horizontal:false,
+            questions:[],
+            currentQuestion:{},
+            fontSizeDesktop: 18,
+            fontSizeTablet: 16,
+            fontSizeMobile: 14,
+            haveImg:false,
+            questionName:'',
+            isNext:true,
+            isBack:false,
+            awsActive:''
+
 		};
 	}
 
     componentDidMount(){
+        this.renArr()
+        this.setState({questions: data, currentQuestion:data[0]})
         if(window.innerWidth < window.innerHeight){
 			this.setState({horizontal: false})
 		}else{
@@ -28,37 +42,82 @@ class Game extends React.Component{
 
     }
 
+    next=()=>{
+        const {currentQuestion, questions}=this.state;
+        var pos = questions.map(function(e) { return e.id; }).indexOf(currentQuestion.id);
+        this.setState({currentQuestion:questions[pos+1]});
+    }
+
+    back=()=>{
+        const {currentQuestion, questions}=this.state;
+        var pos = questions.map(function(e) { return e.id; }).indexOf(currentQuestion.id);
+        this.setState({currentQuestion:questions[pos-1]});
+    }
+
+    finished=()=>{
+
+    }
+
+    selectAnswer=(item)=>{
+        this.setState({awsActive:item})
+    }
+
+    renArr=()=>{
+        var list=[]
+        for (let i = 1; i < 31; i++) {
+            var numberAnswer=this.randomInteger(2,6)+1;
+            var qs={};
+            var aws=[]
+            qs.id=i;
+            
+            if(numberAnswer>4){
+                qs.name=`Câu ${i}: Đây là game nào`;
+                qs.imageUrl=img[this.randomInteger(0, img.length-1)]
+            }else{
+                qs.name=`Câu ${i}: Nội dung câu hỏi số ${i}`;
+                qs.imageUrl="";
+            }
+            qs.duration=30;
+            for (let j = 1; j < numberAnswer; j++) {
+                aws.push(`Đáp án ${j}`)
+            }
+            qs.answers=aws;
+            list.push(qs)
+        }
+        console.log(list)
+    }
+
+    randomInteger=(min, max)=> {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
     render(){
+        const {currentQuestion, awsActive}=this.state;
         return (
-           <div style={{display:'flex', flexDirection:'column'}}>
-                <div>30</div>
-                <div style={{flexDirection:'row'}}>
+           <div style={{display:'flex', flexDirection:'row'}}>
+                <div style={{display:'flex', alignContent:'center'}}>{currentQuestion.duration}</div>
+                <div style={{display:'flex',flexDirection:'column'}}>
                     <div>
-                        <p>Câu:1 Đâu là viết đúng từ CS2?</p>
+                        {currentQuestion.imageUrl && <img src={currentQuestion.imageUrl} width="50%"></img>}
+                        <p>{currentQuestion.name}</p>
                     </div>
-                    <div style={{flexDirection:'column'}}>
-                        <div>
-                            <p>Đáp án 1</p>
-                        </div>
-                        <div>
-                            <p>Đáp án 2</p>
-                        </div>
-                        <div>
-                            <p>Đáp án 3</p>
-                        </div>
-                        <div>
-                            <p>Đáp án 4</p>
-                        </div>
+                    <div id="answer" style={{display:'flex',flexDirection:'row', marginBottom:20}}>
+                        {currentQuestion.answers && currentQuestion.answers.map((item, index) => (
+                            <div key={index}>
+                                <span style={item===awsActive ? {backgroundColor:'green', color:'#fff', fontSize:20, fontWeight:'bold', padding:'7px 15px', marginRight:10, cursor:'pointer'}:{backgroundColor:'gray', color:'#fff', fontSize:20, fontWeight:'bold', padding:'7px 15px', marginRight:10, cursor:'pointer'} } onClick={()=>this.selectAnswer(item)}>{item}</span>
+                            </div>
+                        ))}
                     </div>
-                    <div style={{flexDirection:'column'}}>
+                    <div style={{display:'flex',flexDirection:'row'}}>
                         <div>
-                            <span>Back</span>
+                            <span style={{backgroundColor:'green', color:'#fff', fontSize:20, fontWeight:'bold', padding:'7px 15px', marginRight:10, cursor:'pointer'}} onClick={this.back}>Back</span>
                         </div>
                         <div>
-                            <span>Next</span>
+                            <span style={{backgroundColor:'green', color:'#fff', fontSize:20, fontWeight:'bold', padding:'7px 15px', marginRight:10, cursor:'pointer'}} onClick={this.next}>Next</span>
                         </div>
                         <div>
-                            <span>Kết thúc ngay</span>
+                            <span style={{backgroundColor:'red', color:'#fff', fontSize:20, fontWeight:'bold', padding:'7px 15px', marginRight:10, cursor:'pointer'}} onClick={this.finished}>Kết thúc ngay</span>
                         </div>
                     </div>
                 </div>
---
+++ src/app/ultils/data.js
@@ -0,0 +1,362 @@
+export let data=[
+    {
+        "id": 1,
+        "name": "Câu 1: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/16/b8cb6238e6b849699103e6bc97061a0e.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Câu 2: Nội dung câu hỏi số 2",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 3,
+        "name": "Câu 3: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/16/b8cb6238e6b849699103e6bc97061a0e.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4"
+        ]
+    },
+    {
+        "id": 4,
+        "name": "Câu 4: Nội dung câu hỏi số 4",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 5,
+        "name": "Câu 5: Nội dung câu hỏi số 5",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 6,
+        "name": "Câu 6: Nội dung câu hỏi số 6",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 7,
+        "name": "Câu 7: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/18/8c49de5728ba4e5389e071273f3f6ab0.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 8,
+        "name": "Câu 8: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 9,
+        "name": "Câu 9: Nội dung câu hỏi số 9",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3"
+        ]
+    },
+    {
+        "id": 10,
+        "name": "Câu 10: Nội dung câu hỏi số 10",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 11,
+        "name": "Câu 11: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/15/e33d762fbf4a4d3788d5b8b874bee56b.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4"
+        ]
+    },
+    {
+        "id": 12,
+        "name": "Câu 12: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/15/e33d762fbf4a4d3788d5b8b874bee56b.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 13,
+        "name": "Câu 13: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 14,
+        "name": "Câu 14: Nội dung câu hỏi số 14",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 15,
+        "name": "Câu 15: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/16/b8cb6238e6b849699103e6bc97061a0e.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 16,
+        "name": "Câu 16: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 17,
+        "name": "Câu 17: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/18/8c49de5728ba4e5389e071273f3f6ab0.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 18,
+        "name": "Câu 18: Nội dung câu hỏi số 18",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 19,
+        "name": "Câu 19: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/15/e33d762fbf4a4d3788d5b8b874bee56b.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 20,
+        "name": "Câu 20: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 21,
+        "name": "Câu 21: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4"
+        ]
+    },
+    {
+        "id": 22,
+        "name": "Câu 22: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/18/8c49de5728ba4e5389e071273f3f6ab0.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5",
+            "Đáp án 6"
+        ]
+    },
+    {
+        "id": 23,
+        "name": "Câu 23: Nội dung câu hỏi số 23",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 24,
+        "name": "Câu 24: Nội dung câu hỏi số 24",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 25,
+        "name": "Câu 25: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/15/e33d762fbf4a4d3788d5b8b874bee56b.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4"
+        ]
+    },
+    {
+        "id": 26,
+        "name": "Câu 26: Nội dung câu hỏi số 26",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 27,
+        "name": "Câu 27: Đây là game nào",
+        "imageUrl": "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/18/8c49de5728ba4e5389e071273f3f6ab0.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3",
+            "Đáp án 4",
+            "Đáp án 5"
+        ]
+    },
+    {
+        "id": 28,
+        "name": "Câu 28: Nội dung câu hỏi số 28",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3"
+        ]
+    },
+    {
+        "id": 29,
+        "name": "Câu 29: Nội dung câu hỏi số 29",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2"
+        ]
+    },
+    {
+        "id": 30,
+        "name": "Câu 30: Nội dung câu hỏi số 30",
+        "imageUrl": "",
+        "duration": 30,
+        "answers": [
+            "Đáp án 1",
+            "Đáp án 2",
+            "Đáp án 3"
+        ]
+    }
+]
+
+export let img=[
+    "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/15/e33d762fbf4a4d3788d5b8b874bee56b.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+    "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/18/8c49de5728ba4e5389e071273f3f6ab0.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+    "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/03/16/b8228745da0f4292ae7275778209d4c1.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp",
+    "https://cdn.liveg.vn/content/10002/10002/livestream/thumbnails/2024/05/16/b8cb6238e6b849699103e6bc97061a0e.png_img.webp?sw=300&sh=170&fit=c&exp=1727173682&v=1&ext=webp"
+]
